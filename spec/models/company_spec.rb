413a4bf31b30e62d93d745176f9e1a0b88d8e950
Scott Crawford <scottcrawford03@gmail.com>
2015-01-07 21:38:30 -0700

    finally through the shit storm of changing person

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe Company, :type => :model do
   end
 
   it 'has an array of phone numbers' do
-    expect(company.phone_numbers).to eq([])
+    phone_number = company.phone_numbers.build(number: '333-4444')
+    expect(phone_number.number).to eq('333-4444')
   end
 end
